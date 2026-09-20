0b435b6a8cd1ebeeba0296d94b68e47492f42cbd
Patrícia Serrano <patriciacss@MacBook-Air-de-Patricia.local>
2026-09-20 15:47:38 -0300

    ajustes deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/tomeggs-app/',
+  base: '/tomeggs-v2/', // <--- Altere de 'tomeggs-app' para 'tomeggs-v2'
   plugins: [react()],
 })
